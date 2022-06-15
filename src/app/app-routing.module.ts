ab3706f2d3fd1113b946aa5e112f2f8090bbde02
NotNutNghia <nghiann.vnist@gmail.com>
2022-06-15 21:52:05 +0700

    Build following code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomeCineWebComponent} from "./cine-web/components/home-cine-web/home-cine-web.component";
+import {HomeCineWebComponent} from "./cine-web/components/pages/home-cine-web/home-cine-web.component";
 
 let routes: Routes = [];
 routes = [
   {
     path: 'cine-web',
     component: HomeCineWebComponent
-  }
+  },
+  {
+    path: 'signin',
+    component: HomeCineWebComponent
+  },
+  {
+    path: 'signout',
+    component: HomeCineWebComponent
+  },
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {FlexLayoutModule} from "@angular/flex-layout";
-import {HomeCineWebComponent} from "./cine-web/components/home-cine-web/home-cine-web.component";
+import {HomeCineWebComponent} from "./cine-web/components/pages/home-cine-web/home-cine-web.component";
 import {MatButtonModule} from "@angular/material/button";
 import {MatInputModule} from "@angular/material/input";
 import {MatIconModule} from "@angular/material/icon";
 import {MatExpansionModule} from "@angular/material/expansion";
-import { FooterCineComponent } from './cine-web/components/footer-cine/footer-cine.component';
-import { HeaderCineComponent } from './cine-web/components/header-cine/header-cine.component';
+import { FooterCineComponent } from './cine-web/components/shared/footer-cine/footer-cine.component';
+import { HeaderCineComponent } from './cine-web/components/shared/header-cine/header-cine.component';
 
 
 @NgModule({
