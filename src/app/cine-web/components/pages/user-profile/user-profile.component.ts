f81973461d80e38ef6df408cb188a36812da8323
NotNutNghia <nghiann.vnist@gmail.com>
2022-06-22 15:06:52 +0700

    Add API user profile

--- src/app/cine-web/components/pages/user-profile/user-profile.component.ts
+++ src/app/cine-web/components/pages/user-profile/user-profile.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {UserControllerService} from "../../../cine-svc";
+import * as moment from "moment";
+import {result} from "lodash";
+import * as _ from "lodash";
 
 @Component({
   selector: 'app-user-profile',
@@ -16,14 +19,34 @@ export class UserProfileComponent implements OnInit {
   dataBackup: any
   inProcessChangePass: boolean = false
 
+  avatarUrl: string = ""
+
+  actionEdit: string = ""
+
+  genderChoice = [
+    {
+      value: "MALE",
+      viewValue: "Male"
+    },
+    {
+      value: "FEMALE",
+      viewValue: "Female"
+    },
+    {
+      value: "ANOTHER",
+      viewValue: "Another"
+    }
+  ]
+
   constructor(
     private formBuilder: FormBuilder,
     private userService: UserControllerService
   ) {
     this.userProfile = formBuilder.group({
+      username: [""],
       firstName: ["", Validators.required],
       lastName: ["", Validators.required],
-      gender: ["female", Validators.required],
+      gender: ["", Validators.required],
       dateOfBirth: ["", Validators.required],
       mobile: ["", Validators.required],
       email: ["", Validators.required]
@@ -39,12 +62,15 @@ export class UserProfileComponent implements OnInit {
 
 
     this.passwordForm.disable()
+
+    this.getData()
   }
 
   ngOnInit(): void {
   }
 
   handleUser(actionField: string) {
+    this.actionEdit = actionField
     if(actionField === 'editProfile') {
       this.userProfile.enable()
       this.inProcessEdit = !this.inProcessEdit
@@ -64,4 +90,46 @@ export class UserProfileComponent implements OnInit {
     }
   }
 
+  getData() {
+    this.userService.getCurrentUser().subscribe(result => {
+      if(!result.errorCode) {
+        this.dataBackup = _.cloneDeep(result.result)
+        this.userProfile.patchValue({
+          username: this.dataBackup.username,
+          firstName: this.dataBackup.firstName,
+          lastName: this.dataBackup.lastName,
+          gender: this.dataBackup.gender,
+          dateOfBirth: moment(new Date(this.dataBackup.birthOfDate)).format("yyyy-MM-DD"),
+          mobile: this.dataBackup.mobile,
+          email: this.dataBackup.email
+        })
+
+        console.log()
+
+        this.avatarUrl = `assets/images/users/${this.dataBackup.avatar}`
+      }
+    })
+  }
+
+  editUser() {
+    if(this.actionEdit === 'editProfile') {
+      this.userService.updateUser(
+        this.dataBackup.id,
+        {
+          firstName: this.userProfile.controls['firstName'].value,
+          lastName: this.userProfile.controls['lastName'].value,
+          gender: this.userProfile.controls['gender'].value,
+          birthOfDate: moment(this.userProfile.controls['dateOfBirth'].value).toISOString(),
+          mobile: this.userProfile.controls['mobile'].value,
+          email: this.userProfile.controls['email'].value
+        }
+      ).subscribe(result => {
+        if(!result.errorCode) {
+          this.handleUser("cancel")
+          this.getData()
+        }
+      })
+
+    }
+  }
 }
