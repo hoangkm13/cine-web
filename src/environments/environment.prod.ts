ff68225abf70de9431bb341e766638b41423d2be
hoangkm13 <hoang.workingemail@gmail.com>
2022-06-23 22:40:31 +0700

    test change BE domain

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: " https://demo-cine-svc.herokuapp.com",
+  // apiBaseUrl: "https://demo-cine-svc.herokuapp.com",
+  apiBaseUrl: "http://cine-svc.us-west-2.elasticbeanstalk.com",
 
 };
