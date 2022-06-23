04e593d87dd5e8d1b553de4935e2dfeb6aad9207
hoangkm13 <hoang.workingemail@gmail.com>
2022-06-23 21:07:35 +0700

    test deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: "http://localhost:8001"
-  // apiBaseUrl: "https://demo-cine-svc.herokuapp.com",
+  apiBaseUrl: "http://cinewebappaws-env.eba-7czd7mmf.us-west-2.elasticbeanstalk.com",
 
 };
