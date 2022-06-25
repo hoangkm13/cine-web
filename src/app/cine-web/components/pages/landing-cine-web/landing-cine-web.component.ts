0ca437bfd9a0e19ced0fd6d6005fb8679e3a7ef5
VietTruong-50 <truongquocviet2001@gmail.com>
2022-06-26 02:01:06 +0700

    fix

--- src/app/cine-web/components/pages/landing-cine-web/landing-cine-web.component.ts
+++ src/app/cine-web/components/pages/landing-cine-web/landing-cine-web.component.ts
@@ -45,6 +45,17 @@ export class LandingCineWebComponent implements OnInit {
     outerEdgeLimit: true,
     arrows: true,
     draggable: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+    ]
   };
 
   constructor(
